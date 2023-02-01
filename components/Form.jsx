1fb7bbf6b31776b1f57366139a737621f1b5df80
Flor <florencgmz@gmail.com>
2023-02-01 15:57:05 -0300

    email js

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,30 +1,48 @@
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import { useState } from 'react';
+import { send } from 'emailjs-com';
 
 
 const Form = () => {
 
-    const form = useRef();
+  const [toSend, setToSend] = useState({
+    from_name: '',
+    reply_to: '',
+    message: '',
+  });
+  const [alert, setAlert] = useState(null);
 
-    const sendEmail = (e) => {
-      e.preventDefault();
-  
-      emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
+const onSubmit = (e) => {
+    e.preventDefault();
+    send(
+        'service_ijr1bmb',
+        'template_akwer7k',
+        toSend,
+        'eif9fAisD6xQWjpg5'
+      )
+        .then((response) => {
+          console.log('SUCCESS!', response.status, response.text);
+          setAlert(true)
+        })
+        .catch((err) => {
+          console.log('FAILED...', err);
+          setAlert(true)
         });
-    };
-  
+        };
+
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
+
 
   return (
-    <form className='flex flex-col' ref={form} onSubmit={sendEmail} action="">
+    <form className='flex flex-col' ref={form} onSubmit={onSubmit} action="">
         <label htmlFor="name" className="my-4 text-brown">Nombre</label>
         <input
             className="mb-2 border-b-2 bg-yellowcito border border-green text-brown text-sm focus:ring-greencito focus:border-greencito block w-full p-2.5" 
             id="name"
-            name="user_name"
+            name="from_name"
+            onChange={handleChange}
+            value={toSend.from_name}
             type="text"
             autoComplete="Nombre"
             required
@@ -32,21 +50,30 @@ const Form = () => {
         <label htmlFor="email" className="my-4 text-brown">Email</label>
         <input
             className="mb-4 border-b-2 bg-yellowcito border border-green text-brown text-sm focus:ring-greencito focus:border-greencito block w-full p-2.5 active:ring-green"
-            id="last"
-            name="user_email"
+            id="email"
+            name='reply_to'
+            value={toSend.reply_to}
+            onChange={handleChange}
             type="email"
             autoComplete="Email"
             required
         />
         <label htmlFor="comentario" className="my-4 text-brown">Comentario</label>
-        <textarea id="comentario" name="comentario" rows="4" cols="50" className='mb-4 border-b-2 bg-yellowcito border border-green text-brown text-sm focus:ring-greencito focus:border-greencito block w-full p-2.5'>
+        <textarea 
+          id="comentario" 
+          name="message"
+          value={toSend.message}
+          onChange={handleChange}
+          rows="4" 
+          cols="50" 
+          className='mb-4 border-b-2 bg-yellowcito border border-green text-brown text-sm focus:ring-greencito focus:border-greencito block w-full p-2.5'>
         </textarea>
         <button
         type="submit"
         className="px-12 py-2 border justify-center flex my-2 border-brown shadow text-brown hover:bg-yellow hover:text-green hover:border-green drop-shadow-sm animate-pulse">
         Enviar
         </button>
-    </form>
+    </form>          
   )
 }
 
